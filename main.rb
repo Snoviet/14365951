87be625afde5efd21b05a9ac1aa48d955e09d52c
“Roy <liam.van-der-spek@ucdconnect.ie>
2016-12-01 18:07:37 +0000

    Basic Setup

---
+++ main.rb
@@ -0,0 +1,41 @@
+
+
+class Cell
+
+  def initialize(x, y)
+    @x = x
+    @y = y
+  end
+  
+  def to_s
+
+  end
+
+end
+
+
+class BaseStation
+
+  def initialize(name, cell)
+    @name = name
+    @cell = cell
+  end
+
+  def to_s
+
+  end
+
+end
+
+
+class BaseStations
+
+  def initialize
+    @stationArr = []
+  end
+
+  def to_s
+
+  end
+
+end
