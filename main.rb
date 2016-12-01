23c168da237551fad25bbf5963b9dc616785f9f5
“Roy <liam.van-der-spek@ucdconnect.ie>
2016-12-01 18:21:11 +0000

    file reader

--- main.rb
+++ main.rb
@@ -1,4 +1,32 @@
 
+@mainReader = FileInput.new('base_stations.txt')
+@mainReader.read_in
+
+class FileReader
+
+  def initialize(filename)
+    @fileName = filename
+  end
+
+  def read_in
+    @counter = 0
+    @BaseStation = []
+    @Cell = []
+    lines = File.readlines(@fileName)
+    if !lines.empty?
+      lines.each do |line|
+        temparr = line.split
+        @Cell[@counter] = (Cell.new(temparr[1], temparr[2])
+        @BaseStation[@counter] = (BaseStation.new(temparr[0],@Cell[@counter]))
+        @counter += 1
+      end
+    else
+      @error = "Error: File Empty"
+      puts @error
+    end
+  end
+
+end
 
 class Cell
 
@@ -6,9 +34,9 @@ class Cell
     @x = x
     @y = y
   end
-  
-  def to_s
 
+  def to_s
+    puts "(#{@x},#{@y}"
   end
 
 end
@@ -32,10 +60,16 @@ class BaseStations
 
   def initialize
     @stationArr = []
+    @counter = 0
+  end
+
+  def add_station(station)
+    @stationArr[@counter] = station
+    @counter = @counter + 1
   end
 
   def to_s
-
+    puts
   end
 
 end
